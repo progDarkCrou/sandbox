7c4de94091af5e90616fb9e93136e9c083a7fd34
Andriy Vorona <dark.crou@betacom.com.pl>
2016-01-27 15:13:18 +0100

    help.ts - moved to another dir index.ts - moved to another dir app.build.js - changed due to the moving of the source files

---
+++ www/typescript/ex1/app.build.js
@@ -0,0 +1,14 @@
+/**
+ * Created by avorona on 26.01.16.
+ */
+
+({
+  appDir: ".tmp/src/",
+  baseUrl: "./",
+  dir: ".tmp/build/",
+  paths: {
+    jquery: "empty:",
+    angular: "empty:",
+  },
+  skipModulesInsertion: false
+})
