f659cce6615fcc866ba8b436c05e2bf7188aeae0
Andriy Vorona <dark.crou@betacom.com.pl>
2016-01-27 16:36:40 +0100

    build.js - some minor modification package.json - some minor modification

---
+++ www/typescript/ex1/build.js
@@ -0,0 +1,15 @@
+/**
+ * Created by avorona on 27.01.16.
+ */
+({
+  appDir: ".tmp/compile/",
+  baseUrl: "./",
+  paths: [
+    {
+      jquery: "empty:",
+      angular: "empty:"
+    }
+  ],
+  dir: ".tmp/build/",
+  modules: [{name: "index"}]
+})
