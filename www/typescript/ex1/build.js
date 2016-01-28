bda92fd242b39fc4c6c9accf4081cc50f3d2f4b2
Andriy Vorona <dark.crou@betacom.com.pl>
2016-01-28 12:24:01 +0100

    build.js - fixed config syntax

--- www/typescript/ex1/build.js
+++ www/typescript/ex1/build.js
@@ -4,12 +4,10 @@
 ({
   appDir: ".tmp/compile/",
   baseUrl: "./",
-  paths: [
-    {
-      jquery: "empty:",
-      angular: "empty:"
-    }
-  ],
+  paths: {
+    jquery: "empty:",
+    angular: "empty:"
+  },
   dir: ".tmp/build/",
   modules: [{name: "index"}]
-})
+});
