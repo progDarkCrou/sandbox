72b5966400916d79aa2e8f4f2498f228e8334e5f
Andriy Vorona <dark.crou@betacom.com.pl>
2016-02-08 16:14:12 +0100

    index.ts - some minor changes index.html - added new deps injections bower.json - added new dependency gulpfile.js - added few new tasks package.json - added new dev-dependencies

--- www/typescript/ex1/src/main/js/index.ts
+++ www/typescript/ex1/src/main/js/index.ts
@@ -3,23 +3,23 @@ import {Person} from "./help";
 import * as angular from "angular";
 
 export module Index {
-    var person: Person = new Person('Stiven', 'Pitsburg', 100);
+    console.log('Index module loaded');
+    var person:Person = new Person('Stiven', 'Pitsburg', 100);
 
-    interface Scope extends angular.IScope {}
+    interface Scope extends angular.IScope {
+    }
 
     export var app = angular.module('app', []);
 
     interface IMainControllerScope extends angular.IScope {
         person: Person;
         sayHello(): string;
-        sayHello(firstArg?: string): string;
+        sayHello(firstArg?:string): string;
     }
 
-    app.controller('controller', ['$scope', function ($scope: IMainControllerScope) {
+    app.controller('controller', ['$scope', function ($scope:IMainControllerScope) {
         $scope.person.sayHello();
     }]);
 
-    (function () {
-        console.log(person.sayHello());
-    })();
+    console.log(person.sayHello());
 }
