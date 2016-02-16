3ff8bfcfd7d65859c1e2d68bc17852cb968ad5c2
Andriy Vorona <dark.crou@betacom.com.pl>
2016-02-16 14:08:20 +0100

    BaseTest.java - optimized insert

--- javaSE/spring-data-hibernate/src/test/java/BaseTest.java
+++ javaSE/spring-data-hibernate/src/test/java/BaseTest.java
@@ -1,19 +1,11 @@
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
-import vorona.andriy.MainConfiguration;
-import vorona.andriy.model.House;
-import vorona.andriy.model.User;
 import vorona.andriy.repositories.CityRepository;
 import vorona.andriy.repositories.HouseRepository;
 import vorona.andriy.repositories.UserRepository;
-
-import java.util.List;
 
 /**
  * Created by avorona on 28.12.15.
