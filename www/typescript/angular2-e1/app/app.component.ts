c1dc95cd7f153fa9b076d44c62e41580ce13a2af
Andriy Vorona <dark.crou@betacom.com.pl>
2016-03-02 10:26:14 +0100

    come minor changes

--- www/typescript/angular2-e1/app/app.component.ts
+++ www/typescript/angular2-e1/app/app.component.ts
@@ -1,6 +1,19 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+import {NgFor} from 'angular2/common';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: '/templates/app.component.html',
+  directives: [NgFor]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  elements:any[];
+
+  ngOnInit():any {
+    this.elements = [
+      1, 2, 3, 4
+    ];
+    console.log('Hello from the app.component');
+  }
+}
