5b528696c048b04261f7e59b5c2cb3ef5a1b31ae
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-06 10:40:23 +0200

    cert-generator.sh - Improved config generating

--- vpn/certs/cert-generator.sh
+++ vpn/certs/cert-generator.sh
@@ -18,20 +18,20 @@ do
 done
 
 if [[ "$#" -ge 2 ]]; then
-	TMP_CONFIG="$(sudo cat $CONFIG_PATH)"
-	TMP_CONFIG="$TMP_CONFIG $(printf '\n[SAN]\nsubjectAltName=IP:'$2'\n')"
+	TMP_CONFIG="$(printf '\n[SAN]\nsubjectAltName=IP:'$2'\n')"
 	
-	CONFIG=''
-
-	for ip in ${@:2}; do
-		CONFIG="$TMP_CONFIG $(printf '\nsubjectAltName=IP:'$ip)"
+	for ip in ${@:3}; do
+		TMP_CONFIG="$TMP_CONFIG $(printf '\nsubjectAltName=IP:'$ip)"
 	done
 
-	echo 'New config'
-	echo "$CONFIG" | tail -n $(( $# - 1 ))
+	echo "New config"
+	echo "$TMP_CONFIG"
 
 	echo 
 	echo "*** Generating new key... ***"
+
+	CONFIG="$(sudo cat $CONFIG_PATH)$TMP_CONFIG"
+
 	openssl genrsa -out "$NAME-priv-key.pem" 2048; 
 	openssl req -subj "/CN=$NAME" -new -key "$NAME-priv-key.pem" -out "$NAME.csr" -reqexts SAN -config <(echo "$CONFIG"); 
 	openssl x509 -req -days 1825 -in "$NAME.csr" -CA ca.pem -CAkey ca-prib-key.pem -CAcreateserial -out "$NAME-cert.pem" -extensions v3_req -extfile <(echo "$CONFIG"); 
