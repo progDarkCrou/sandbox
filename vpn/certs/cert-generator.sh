905ad5459d7ad3d1dab365b810ecf4aabf909663
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-06 10:47:02 +0200

    cert-generator.sh - some minor improvements

--- vpn/certs/cert-generator.sh
+++ vpn/certs/cert-generator.sh
@@ -3,7 +3,6 @@
 
 NAME="$1"
 
-
 if [[ -z "$NAME" ]]; then
 	echo "Please provide name of the certificate as first agrument"
 	exit 1;
@@ -17,23 +16,26 @@ do
 	read -i  "Please provide the route to the configuration file: " CONFIG_PATH;
 done
 
+TMP_CONFIG=""
+
 if [[ "$#" -ge 2 ]]; then
-	TMP_CONFIG="$(printf '\n[SAN]\nsubjectAltName=IP:'$2'\n')"
+	TMP_CONFIG="$(printf '[SAN]\nsubjectAltName=IP:'$2'\n')"
 	
 	for ip in ${@:3}; do
 		TMP_CONFIG="$TMP_CONFIG $(printf '\nsubjectAltName=IP:'$ip)"
 	done
 
-	echo "New config"
+	echo "*** Alternative names will be added to the config ***"
 	echo "$TMP_CONFIG"
-
-	echo 
-	echo "*** Generating new key... ***"
-
-	CONFIG="$(sudo cat $CONFIG_PATH)$TMP_CONFIG"
-
-	openssl genrsa -out "$NAME-priv-key.pem" 2048; 
-	openssl req -subj "/CN=$NAME" -new -key "$NAME-priv-key.pem" -out "$NAME.csr" -reqexts SAN -config <(echo "$CONFIG"); 
-	openssl x509 -req -days 1825 -in "$NAME.csr" -CA ca.pem -CAkey ca-prib-key.pem -CAcreateserial -out "$NAME-cert.pem" -extensions v3_req -extfile <(echo "$CONFIG"); 
-	openssl rsa -in "$NAME-priv-key.pem" -out "$NAME-priv-key.pem";
 fi
+
+echo
+echo "*** Generating new key... ***"
+
+CONFIG="$(sudo cat $CONFIG_PATH)$(printf '\n'$TMP_CONFIG)"
+
+openssl genrsa -out "$NAME-priv-key.pem" 2048 > /dev/null; 
+openssl req -subj "/CN=$NAME" -new -key "$NAME-priv-key.pem" -out "$NAME.csr" -reqexts SAN -config <(echo "$CONFIG") > /dev/null; 
+openssl x509 -req -days 1825 -in "$NAME.csr" -CA ca.pem -CAkey ca-prib-key.pem -CAcreateserial -out "$NAME-cert.pem" -extensions v3_req -extfile <(echo "$CONFIG") > /dev/null;
+
+echo "*** Generation keys ended. ***"
