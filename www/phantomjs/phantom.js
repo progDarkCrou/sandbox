8f0bbbc1e211a4cf22ba2e496253ace128f67e0b
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-12 11:57:28 +0200

    v1.0.0 - phantom.js

--- www/phantomjs/phantom.js
+++ www/phantomjs/phantom.js
@@ -6,44 +6,49 @@ if (system.args.length === 1) {
   phantom.exit(1);
 }
 
-address = system.args[1];
+var address = system.args[1];
 
-var urlPattern = /^http(s)?\:\/\/([a-z0-9]{2,}\.){2,}/i;
+var urlPattern = /^http(s)?\:\/\/([a-z0-9]{1,}\.){1,}[a-z0-9]/i;
+
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
 
 page.open(address, function(status) {
   if (status !== 'success') {
     console.log('Failed to load the address: ' + address);
     phantom.exit(1);
   } else {
-    // page.evaluate(function () {
-    //   var config = {
-    //     host: window.location.host,
-    //     protocol: window.location.protocol
-    //   }
+    page.evaluate(function (urlPatternString, urlCompletionFunc) {
+      eval(urlCompletionFunc);
+      var urlPattern = new RegExp(urlPatternString);
 
-    //   if ($ || window.$) {
-    //     $('link').filter(function(n, elem) {
-    //       var href = $(elem).prop('href');
-    //       return !!href? !urlPattern.test(href): false;
-    //     }).each(urlCompletion('href'));
-    //     $('script').filter(function(n, elem) {
-    //       var src = $(elem).prop('src');
-    //       return !!src? !urlPattern.test(src): false;
-    //     }).each(urlCompletion('src'));
-    //   }
-    // });
+      var config = {
+        host: window.location.host,
+        protocol: window.location.protocol
+      };
+
+      if ($ || window.$) {
+        $('link[href]').filter(function(n, elem) {
+          var href = elem.attributes.href.value;
+          return !!href? !urlPattern.test(href): false;
+        }).each(urlCompletion('href', config));
+        $('script[src]').filter(function(n, elem) {
+          var src = elem.attributes.src.value;
+          return !!src? !urlPattern.test(src): false;
+        }).each(urlCompletion('src', config));
+      }
+    }, urlPattern.source, urlCompletion.toString());
     console.log(page.content);
     phantom.exit(0);
   }
 });
 
 function urlCompletion(attrName, config) {
-  function (n, elem) {
-    var $this = $(elem);
-    var attr = $this.prop(attrName);
-
-    if (attr && urlPattern.test(attr)) {
-      $this.prop(attrName, config.protocol + '//' + config.host + (attr.charAt(0) === '/'?'':'/') + attr);
+  return function (n, elem) {
+    var attr = elem[attrName];
+    if (attr && !urlPattern.test(attr)) {
+      elem[attrName] = config.protocol + '//' + config.host + (attr.charAt(0) === '/'?'':'/') + attr;
     }
-  }
-}
+  };
+};
