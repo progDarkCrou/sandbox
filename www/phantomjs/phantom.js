21e5458711d571295c5f1befbe021459a9f0cbdb
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-12 10:28:20 +0200

    Improved page link and script urls completion

--- www/phantomjs/phantom.js
+++ www/phantomjs/phantom.js
@@ -7,13 +7,43 @@ if (system.args.length === 1) {
 }
 
 address = system.args[1];
+
+var urlPattern = /^http(s)?\:\/\/([a-z0-9]{2,}\.){2,}/i;
+
 page.open(address, function(status) {
-  setTimeout(function() {
-    if (status !== 'success') {
-      console.log('Failed to load the address: ' + address);
-    } else {
-      console.log(page.content);
-    }
-    phantom.exit();
-  }, 10000);
+  if (status !== 'success') {
+    console.log('Failed to load the address: ' + address);
+    phantom.exit(1);
+  } else {
+    // page.evaluate(function () {
+    //   var config = {
+    //     host: window.location.host,
+    //     protocol: window.location.protocol
+    //   }
+
+    //   if ($ || window.$) {
+    //     $('link').filter(function(n, elem) {
+    //       var href = $(elem).prop('href');
+    //       return !!href? !urlPattern.test(href): false;
+    //     }).each(urlCompletion('href'));
+    //     $('script').filter(function(n, elem) {
+    //       var src = $(elem).prop('src');
+    //       return !!src? !urlPattern.test(src): false;
+    //     }).each(urlCompletion('src'));
+    //   }
+    // });
+    console.log(page.content);
+    phantom.exit(0);
+  }
 });
+
+function urlCompletion(attrName, config) {
+  function (n, elem) {
+    var $this = $(elem);
+    var attr = $this.prop(attrName);
+
+    if (attr && urlPattern.test(attr)) {
+      $this.prop(attrName, config.protocol + '//' + config.host + (attr.charAt(0) === '/'?'':'/') + attr);
+    }
+  }
+}
