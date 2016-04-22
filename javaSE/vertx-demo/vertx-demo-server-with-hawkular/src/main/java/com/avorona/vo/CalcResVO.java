19569b94ac9da7ceafb2fb13d51af422bd6abb5a
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-22 15:42:52 +0200

    Vertx Demo project added - math progression calculator

---
+++ javaSE/vertx-demo/vertx-demo-server-with-hawkular/src/main/java/com/avorona/vo/CalcResVO.java
@@ -0,0 +1,21 @@
+package com.avorona.vo;
+
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Created by avorona on 21.04.16.
+ */
+public class CalcResVO {
+    private JsonObject json = new JsonObject();
+
+    public CalcResVO(Long from, Long to, Long res) {
+        json.put("from", from);
+        json.put("to", to);
+        json.put("res", res);
+    }
+
+    @Override
+    public String toString() {
+        return json.toString();
+    }
+}
