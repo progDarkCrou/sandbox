537eff1805497da1a76bc4414e95163a9eed0004
Andriy Vorona <dark.crou@betacom.com.pl>
2016-04-28 14:54:37 +0200

    Added custom authentication provider, and custom user details service

--- javaSE/spring-web-security/src/main/java/vorona/andriy/controller/MainController.java
+++ javaSE/spring-web-security/src/main/java/vorona/andriy/controller/MainController.java
@@ -1,37 +1,25 @@
 package vorona.andriy.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import vorona.andriy.SessionHolder;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by avorona on 29.12.15.
  */
-@Controller
+@RestController
 @CrossOrigin(methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE})
 public class MainController {
 
-    @Autowired
-    private SessionHolder sessionHolder;
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void login(Authentication authentication, HttpServletResponse response) {
-        response.addCookie(new Cookie("X-SID", sessionHolder.generateAndSave((String) authentication.getPrincipal())));
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/sayhello", method = RequestMethod.GET)
-    @PreAuthorize("hasRole('ADMIN')")
-    public String hello(Authentication authentication) {
-        return "Hello - " + authentication.getPrincipal();
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public Map<String, String> user(Authentication auth) {
+        Map<String, String> userDetails = new TreeMap<>();
+        userDetails.put("username", auth.getPrincipal().toString());
+        return userDetails;
     }
 }
